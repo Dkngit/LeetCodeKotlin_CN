c0354c48a590fc7ba60c1c55ba6fd56fbab79591
Dkngit <lhc2014v@outlook.com>
2022-09-30 09:11:38 +0800

    从kotlin_base项目迁移

---
+++ src/main/java/kotlin_base/First.kt
@@ -0,0 +1,14 @@
+package kotlin_base
+
+fun main() {
+    println("Hello World")
+    val a:Int=1
+    val b=2
+
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum1(a: Int, b: Int) = a + b
---
+++ src/main/java/kotlin_base/myFile/CopyAndRename.kt
@@ -0,0 +1,49 @@
+package hello.myFile
+
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.regex.Pattern
+
+fun main() {
+    val directory = File("D:\\WorkSpace\\FileTest\\jpegs")
+//        File directory2 = new File("D:\\WorkSpace\\FileTest\\jpegs2");
+    //        File directory2 = new File("D:\\WorkSpace\\FileTest\\jpegs2");
+    val files = directory.listFiles() ?: throw NoSuchElementException()
+
+    for (file in files) {
+        println(file.name)
+    }
+    val pattern = Pattern.compile("^0(\\((\\d+)\\))?")
+
+    val files1 = arrayOfNulls<File>(files.size)
+    for (file in files) {
+        val name = file.name
+        val matcher = pattern.matcher(name)
+        if (matcher.find()) {
+            var index = -1
+            val group = matcher.group(2)
+            index = group?.toInt() ?: 0
+            if (index < 0) {
+                println("group error: $name")
+                continue
+            }
+            if (files1[index] != null) {
+                println("group has: $name")
+                continue
+            }
+            files1[index] = file
+        }
+    }
+
+    for ((i,file) in files1.withIndex()) {
+        if (file == null)
+            continue;
+        val inputStream = FileInputStream(file)
+        val fileOutputStream = FileOutputStream("D:\\WorkSpace\\FileTest\\jpegs2" + File.separator + i + ".jpg")
+        inputStream.copyTo(fileOutputStream)
+        fileOutputStream.flush()
+        fileOutputStream.close()
+        inputStream.close()
+    }
+}
